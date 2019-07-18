f26c489d26864b77bb1927480346bf9ddd398223
fcarlone <frankcarlone@gmail.com>
2019-07-18 16:55:35 -0400

    update server.js file

--- server.js
+++ server.js
@@ -51,12 +51,12 @@ require("./routes/favorite")(app);
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  // Route to load single HTML page
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
 }
+
+// Route to load single HTML page
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Add listner
 app.listen(PORT, () => {
